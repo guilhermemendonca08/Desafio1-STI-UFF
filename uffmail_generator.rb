dbc9e77b2ca3e7bf4b7fb613db8c7089c3826479
guilhermemendonca08 <guilhermepm@id.uff.br>
2024-12-08 23:23:17 -0300

    Mudança em uffmail_generator

--- uffmail_generator.rb
+++ uffmail_generator.rb
@@ -1,25 +1,25 @@
 module UffmailGenerator
 
-    def self.generate_uffmail_options(student_name)
-      student_name = student_name.downcase
-      student_name = student_name.split(' ')
+  def self.generate_uffmail_options(student_name)
+    student_name = student_name.downcase
+    student_name = student_name.split(' ')
 
-      first_name = student_name[0]
-      middle_name = student_name[1]
-      last_name = student_name[2]
+    first_name = student_name[0]
+    middle_name = student_name[1]
+    last_name = student_name[2]
 
-      first_letter_first_name = student_name[0][0]
-      first_letter_middle_name = student_name[1][0]
-      first_letter_second_name = student_name[2][0]
+    first_letter_first_name = first_name[0]
+    first_letter_middle_name = middle_name[0]
+    first_letter_second_name = last_name[0]
 
-      options = [
-        "#{first_name}_#{middle_name}@id.uff.br",
-        "#{first_name}#{first_letter_middle_name}#{first_letter_second_name}@id.uff.br",
-        "#{first_name}#{last_name}@id.uff.br",
-        "#{first_letter_first_name}#{last_name}@id.uff.br",
-        "#{first_letter_first_name}#{middle_name}#{last_name}@id.uff.br"
-      ]
+    options = [
+    "#{first_name}_#{middle_name}@id.uff.br",
+    "#{first_name}#{first_letter_middle_name}#{first_letter_second_name}@id.uff.br",
+    "#{first_name}#{last_name}@id.uff.br",
+    "#{first_letter_first_name}#{last_name}@id.uff.br",
+    "#{first_letter_first_name}#{middle_name}#{last_name}@id.uff.br"
+    ]
 
-      options
-    end
+    options
+  end
 end
